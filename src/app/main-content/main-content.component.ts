68809dde8e853246d7a48295ad8ad0c95749a603
s4mastig <maximilian.stigler@gmx.net>
2024-10-03 16:09:36 +0200

    skills-section completed

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AotComponent } from './aot/aot.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from "./skills/skills.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AotComponent, AboutMeComponent, SkillsComponent],
+  imports: [AotComponent, AboutMeComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
